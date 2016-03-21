bc851aa42bf24f180da3afeeff981f5af1f5bdbe
ansel.simon <simon.ansel@outlook.com>
2016-03-21 14:58:07 +0100

    Exercice 4

--- application.js
+++ application.js
@@ -16,10 +16,10 @@ function runChallenges() {
 
   // Ex 3. Replace the email hint (next to the input) with 'This is my email now'
   //       The text should be emphasized using a <strong> tag
-  $('#email-hint').html("<strong>This is my email now</strong>")
+  $('#email-hint').html("<strong>This is my email now</strong>");
 
   // Ex 4. Add the .blue CSS class to the table header cells (th elements)
-
+  $("th").addClass('blue');
 
 
   // Ex 5. Count the number of table body rows there are (team count!)
