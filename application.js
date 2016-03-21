78bf72a1021e1e25f6a9f36eb210316c2b2a0a0f
blackcataleya <elodieandrieux62100@gmail.com>
2016-03-21 15:07:10 +0100

    Exo3

--- application.js
+++ application.js
@@ -8,7 +8,7 @@
 function runChallenges() {
 
   // Ex 1. Read the content of the email input
-  var email = "dleuliette@gmail.com";  // TODO: replace null, keep the email variable.
+    var email = "dleuliette@gmail.com";  // TODO: replace null, keep the email variable.
 
 
   // Ex 2. Fill the content of the email input with your email
@@ -17,7 +17,7 @@ function runChallenges() {
 
   // Ex 3. Replace the email hint (next to the input) with 'This is my email now'
   //       The text should be emphasized using a <strong> tag
-
+    $("#email-hint").html('<strong>This is my email now</strong>');
 
   // Ex 4. Add the .blue CSS class to the table header cells (th elements)
 
