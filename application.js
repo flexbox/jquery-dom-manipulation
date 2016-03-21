bf9fd39eadb9b81f7a8b3e35f5e2d42e6f9b7b49
Ludovic Menard <menard.ludovic@outlook.fr>
2016-03-21 14:50:26 +0100

    Exercise 4

--- application.js
+++ application.js
@@ -21,7 +21,7 @@ function runChallenges() {
   $('#email-hint').html("<strong>This is my email now</strong>");
 
   // Ex 4. Add the .blue CSS class to the table header cells (th elements)
-
+  $('th').addClass('blue');
 
 
   // Ex 5. Count the number of table body rows there are (team count!)
