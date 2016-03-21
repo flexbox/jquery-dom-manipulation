5878576c587e5e664c5035c3d7ef76031606769e
Sif <aurelien.ducloy@outlook.fr>
2016-03-21 16:22:09 +0100

    Exercice 7

--- application.js
+++ application.js
@@ -8,30 +8,31 @@
 function runChallenges() {
 
   // Ex 1. Read the content of the email input
-  var email = null;  // TODO: replace null, keep the email variable.
+  var email = 'dleuliette@gmail.com';  // TODO: replace null, keep the email variable.
 
 
   // Ex 2. Fill the content of the email input with your email
-
-
+ document.getElementById("email").value ="aurelien.ducloy@outlook.fr";
 
   // Ex 3. Replace the email hint (next to the input) with 'This is my email now'
   //       The text should be emphasized using a <strong> tag
+  document.getElementById("email-hint").innerHTML = "This is my email now";
+  $('email-hint').html('<strong> This is my email now </strong>');
+  //$("strong").text(email-hint);
 
 
   // Ex 4. Add the .blue CSS class to the table header cells (th elements)
-
+  $("th").addClass("blue");
 
 
   // Ex 5. Count the number of table body rows there are (team count!)
-  var team_count = 0;  // TODO: replace 0, keep the team_count variable.
-
+  var team_count = $(".table > tbody > tr").length;  // TODO: replace 0, keep the team_count variable.
+  
 
 
   // Ex 6. Say there is a 15th team added to the table.
   //       Add a row at the bottom, this new team should have zero points.
-
-
+  $('.table > tbody').append('<tr><td>15</td><td>UneNouvelleTeam</td><td>0</td></tr>')
 
   // Ex 7. Write some code to sum all points given to all teams
   var sum = 0;  // TODO: replace 0 with your sum result, keep the sum variable.
@@ -39,7 +40,7 @@ function runChallenges() {
 
 
   // Ex 8. Change the background color of all table header cells to #DDF4FF
-
+  $("th").css("background-color","rgb(221,244,255");
 
 
   // Ex 9. Translate the #results list 20px downward (animation would be great ^_^)
@@ -47,7 +48,7 @@ function runChallenges() {
 
 
   // Ex 10. Remove the "Email:" label from the DOM
-
+  $('label').remove();  
 
 
 
