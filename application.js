a91c1bc65dd48c8cbdd0b07cdd851dfe2eb53c21
Antoine Thompson <toine.thompson@gmail.com>
2016-05-07 17:26:53 +0200

    Exercise 6

--- application.js
+++ application.js
@@ -32,7 +32,8 @@ function runChallenges() {
 
   // Ex 6. Say there is a 15th team added to the table.
   //       Add a row at the bottom, this new team should have zero points.
-
+  var row = $('<tr>').html('<td>' + (team_count + 1) + '</td><td>New team</td><td>0</td></tr>');
+  $('tbody').append(row);
 
 
   // Ex 7. Write some code to sum all points given to all teams
